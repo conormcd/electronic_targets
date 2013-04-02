7e018587807d9c22f0bc04f8e25ae12a4ab3624c
Conor McDermottroe <conor@mcdermottroe.com>
2013-04-02 11:46:54 +0100

    Clean up spec/shooting_stats/shooting_stats_spec.rb

--- spec/shooting_stats/shooting_stats_spec.rb
+++ spec/shooting_stats/shooting_stats_spec.rb
@@ -2,58 +2,56 @@ require 'spec_helper'
 
 require 'shooting_stats'
 
-def bad_command_lines
-  [
-    [sample_mlq],
-    ['--foo', sample_mlq, 'list'],
-  ]
-end
-
-def good_command_lines
-  [
-    [sample_mlq, 'list'],
-    ['-t', 'mlq', sample_mlq, 'list'],
-    [sample_mlq, '-t', 'mlq', 'list'],
-  ]
-end
-
 def sample_mlq
-  @sample_mlq = SpecHelper::Fixtures::DataSource::Megalink.new.mlq.values.first
-  if @sample_mlq =~ /^#{Dir.pwd}/
-    @sample_mlq = @sample_mlq.sub(/^#{Dir.pwd}\/?/, '')
+  sample_mlq = SpecHelper::Fixtures::DataSource::Megalink.new.mlq.values.first
+  if sample_mlq =~ /^#{Dir.pwd}/
+    sample_mlq = sample_mlq.sub(/^#{Dir.pwd}\/?/, '')
   end
+  sample_mlq
 end
 
 describe ShootingStats::Runner do
-  describe '#configure_from!' do
-    before do
-      @runner = ShootingStats::Runner.new
-    end
+  describe '#configure!' do
+    shared_examples '#configure!' do |good_or_bad, args|
+      args_string = args.empty? ? 'no arguments' : args.join(' ')
+      context "when given #{args_string}" do
+        before do
+          @runner = ShootingStats::Runner.new(args)
+        end
 
-    describe 'does not raise an error when given good arguments' do
-      good_command_lines.each do |args|
-        it "#{args.join(' ')}" do
-          expect {
-            @runner.configure_from!(args)
-          }.to_not raise_error
+        it 'sets the help message' do
+          begin
+            @runner.configure!
+          rescue OptionParser::ParseError
+          ensure
+            @runner.help_message.should_not be_nil
+          end
+        end
+
+        if good_or_bad == :good
+          it 'does not raise an OptionParser::ParseError' do
+            expect {
+              @runner.configure!
+            }.to_not raise_error OptionParser::ParseError
+          end
+        else
+          it 'raises an OptionParser::ParseError' do
+            expect {
+              @runner.configure!
+            }.to raise_error OptionParser::ParseError
+          end
         end
       end
     end
 
-    describe 'raises an OptionParser::ParseError when given bad arguments' do
-      it '(no arguments)' do
-        expect {
-          @runner.configure_from!([])
-        }.to raise_error OptionParser::ParseError
-      end
-
-      bad_command_lines.each do |args|
-        it "#{args.join(' ')}" do
-          expect {
-            @runner.configure_from!(args)
-          }.to raise_error OptionParser::ParseError
-        end
-      end
-    end
+    it_should_behave_like '#configure!', :good, [sample_mlq, 'list']
+    it_should_behave_like '#configure!', :good,
+      ['-t', 'mlq', sample_mlq, 'list']
+    it_should_behave_like '#configure!', :good,
+      [sample_mlq, '-t', 'mlq', 'list']
+    it_should_behave_like '#configure!', :bad, []
+    it_should_behave_like '#configure!', :bad, [sample_mlq]
+    it_should_behave_like '#configure!', :bad,
+      ['--foo', sample_mlq, 'list']
   end
 end
